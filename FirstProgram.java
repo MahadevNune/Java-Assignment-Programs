e52efe70f9dfa505d6ab816f447eecf875efabbf
Mahadev Nune <nunemahadev@gmail.com>
2025-04-05 00:47:03 +0530

    First Java File Commit

---
+++ FirstProgram.java
@@ -0,0 +1,12 @@
+package day1_5142024;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("first program");
+ 
+		// TODO Auto-generated method stub
+
+	}
+
+}
